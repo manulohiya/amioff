556962ba69cf97b29dad864b5e11528f9909ba09
Neil Maheshwari <Neil@neils-mbp.gateway.sonic.net>
2016-04-02 00:52:30 -0700

    Adds request library

--- test/test.js
+++ test/test.js
@@ -1,10 +1,19 @@
 var assert = require('assert');
+var timesheet = require('../lib/amionService');
 
 describe('Array', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
+
+describe('TestFunction', function () {
+  describe('myTest', function () {
+    it('should be ok', function () {
+      assert.equal(timesheet.requestTimesheet(), 1);
     });
   });
 });
