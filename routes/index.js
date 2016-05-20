ffdfa25734a2872900a9a7da5d65841e65af5805
Neil Maheshwari <Neil@Neils-MacBook-Pro.local>
2016-05-19 22:55:32 -0700

    Parsing amion via papaparse

--- routes/index.js
+++ routes/index.js
@@ -39,7 +39,7 @@ router.get("/api/hang", function (req, res) {
 	2015,
 	function(body) {
 	    console.log("******************************");
-	    console.log(timesheet.attachHeadersToTimesheet(timesheet.cleanTimesheet(body)));
+	    console.log(timesheet.parseToJson(timesheet.attachHeadersToTimesheet(timesheet.cleanTimesheet(body))));
 	    console.log("******************************");
 	    console.log("WE MADE IT")
 	});
