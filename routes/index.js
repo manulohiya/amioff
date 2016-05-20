76dc93b8c5ed10c88cf30144c73eddc7a36fa38e
Neil Maheshwari <Neil@Neils-MacBook-Pro.local>
2016-05-19 23:32:30 -0700

    Hang route passes param to amion

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var timesheet = require('../lib/amionService');
+var moment = require('moment');
+var tz = require('moment-timezone');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -30,13 +32,15 @@ var residents = [
 // });
 
 router.get("/api/hang", function (req, res) {
-  console.log("Query function is running")
-  var query = req.query;
-    
+    console.log("Query function is running")
+    var query = req.query;
+    var time = query.time;
+    // TODO: Make time calculations more robust
+    var pstTime = moment.unix(time).clone().tz("America/Los_Angeles");
     timesheet.requestTimesheet(
-	"UCSFEM",
-	3,
-	2015,
+	query.programName,
+	pstTime.format("M"),
+	pstTime.format("YYYY"),
 	function(body) {
 	    res.json(timesheet.parseToJson(timesheet.attachHeadersToTimesheet(timesheet.cleanTimesheet(body))));
 	    console.log("Handled query for query req");
